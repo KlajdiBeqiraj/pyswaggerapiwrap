06b6b221c33c0cca270671cc662704b7ceb218f2
KlajdiBeqiraj <k.beqiraj@myw.ai>
2024-08-01 15:21:13 +0200

    Change the name of the repository

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="PySwaggerAPIWrap",
+    name="pyswaggerapiwrap",
     version="0.1.4",
     description="A Python wrapper for API services that enables Swagger integration.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Klajdi Beqiraj",
     author_email="klajdibeqiraj96@gmail.com",
-    url="https://github.com/KlajdiBeqiraj/PySwaggerAPIWrap.git",
+    url="https://github.com/KlajdiBeqiraj/pyswaggerapiwrap.git",
     packages=find_packages(),
     install_requires=[],
     classifiers=[
