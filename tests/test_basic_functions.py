dddeda8420b487c18c5085ac332b5a33f983ee01
KlajdiBeqiraj <k.beqiraj@myw.ai>
2024-08-01 14:47:18 +0200

    Created the test for the package

---
+++ tests/test_basic_functions.py
@@ -0,0 +1,71 @@
+"""
+Test base functions and classes
+"""
+
+from typing import Dict
+
+import pandas as pd
+
+from PySwaggerAPIWrap.api_filter import APIDataFrameFilter
+from PySwaggerAPIWrap.api_route import APIRoute
+from PySwaggerAPIWrap.http_client import HttpClient
+
+ENDPOINT = "https://petstore.swagger.io/v2"
+AUTH_TOKEN = "special-key"
+
+HTTP_CLIENT_OBJ = HttpClient(base_url=ENDPOINT, auth_token=AUTH_TOKEN)
+ROUTES_DICT = HTTP_CLIENT_OBJ.get_routes_df(swagger_route="/swagger.json")
+API_FILTER = APIDataFrameFilter(ROUTES_DICT)
+
+
+def test_get_swagger_dataframe():
+    """
+    Check if the routes df is generated
+    :return:
+    """
+    routes_dict_2 = HTTP_CLIENT_OBJ.get_routes_df(swagger_route="/swagger.json")
+    print(isinstance(routes_dict_2, pd.DataFrame))
+    assert isinstance(routes_dict_2, pd.DataFrame)
+    assert routes_dict_2.shape[0] > 0
+
+
+def test_api_filter():
+    """
+
+    :return:
+    """
+    api_filter_2 = APIDataFrameFilter(ROUTES_DICT)
+    assert api_filter_2.df_object.shape[0] > 0
+
+
+def test_attribute_indexing():
+    """
+
+    :return:
+    """
+
+    api_selected = API_FILTER.store.get_inventory
+
+    assert isinstance(api_selected, APIRoute)
+
+
+def test_filter_method():
+    """
+
+    :return:
+    """
+
+    df_result = API_FILTER.filter(method="GET", route_pattern="find", api_type="pet")
+    assert isinstance(df_result, pd.DataFrame)
+    assert df_result.shape[0] > 0
+
+
+def test_run_api():
+    """
+
+    :return:
+    """
+
+    data = API_FILTER.pet.get_with_petId.run(http_client=HTTP_CLIENT_OBJ, petId=1)
+    assert isinstance(data, Dict)
+    assert len(list(data.keys())) > 0
---
+++ tests/test_status.py
@@ -0,0 +1,30 @@
+"""
+test the saving and loading the status
+"""
+
+import os
+
+from PySwaggerAPIWrap.status import load_status, save_status
+from tests.test_basic_functions import API_FILTER, HTTP_CLIENT_OBJ, ROUTES_DICT
+
+
+def test_get_swagger_dataframe():
+    """
+    :return:
+    """
+    file_path = os.path.join("resources", "saved_data", "test_status.psw")
+
+    # remove the previous status
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+    # save the status
+    save_status(file_path, http_client=HTTP_CLIENT_OBJ, routes_dict=ROUTES_DICT)
+
+    # load the status
+    new_api_filter, new_http_client = load_status(file_path)
+
+    # compare the result
+    data_original_api = API_FILTER.store.get_inventory.run(HTTP_CLIENT_OBJ)
+    data_reloaded_api = new_api_filter.store.get_inventory.run(new_http_client)
+    assert data_original_api == data_reloaded_api
